9a923f7097b4201052d22847ba5873e9ba9e4652
David Latorre <david@latorredev.com>
2020-04-12 16:15:44 -0500

    [ADD] anadido folder para ejercicios de fundamentos de python

---
+++ ejercicios-bases-pyhton/01-ejercicio.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Primer ejercicio de fundamentos de python
+
+"""
+Crear dos variables
+Una pais y otra continente
+mostrar su valor por pantalla
+poner un comentario al lado de cada variable
+para decir que tipo de dato es.
+"""
+
+pais = input("De que pais eres?: ") # Tipo str
+continente = input("De que continente eres?: ") # tipo str 
+
+print(f"Saludos, eres del pais {pais} que esta en el continente de {continente}")
