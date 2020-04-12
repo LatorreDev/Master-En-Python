2989854e65cc687ae6ce49744c7fa4bf0c397909
David Latorre <david@latorredev.com>
2020-04-12 17:18:18 -0500

    [ADD] anadido ejercicio 4 de fundamentos de python

---
+++ ejercicios-bases-pyhton/04-ejercicio.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Ejercicio 4 de fundamentos de python
+
+"""
+
+Pedir dos numeros al usuario y hacer todas las operaciones basicas
+de calculadora, mostrarlo en pantalla.
+
+"""
+
+num1 = int(input("Ingresa el primer numero: "))
+num2 = int(input("Ingresa el segundo numero: "))
+
+print("#### Calculadora ####")
+print(f"La suma de los numeros es: {num1 + num2}")
+print(f"La resta de los numeros es: {num1 - num2}")
+print(f"La multiplicaion de los numeros es: {num1 * num2}")
+print(f"La division de los numeros es: {num1 / num2}")
