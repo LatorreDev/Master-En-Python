52ba5b318240848c44fa7b67e817001be351aa7d
David Latorre <david@latorredev.com>
2020-04-19 18:04:04 -0500

    [ADD] anadido script explicativo del return en python

---
+++ Funciones/015-Return.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Return con python3
+
+# Return equivale a devolver datos
+
+def saludame(nombre):
+    saludo = f"Hola {nombre}, saludos"
+
+    return saludo
+
+print(saludame("David"))
