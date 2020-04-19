f9a661045cefe4283bf2846dc2647347ff5ff094
David Latorre <david@latorredev.com>
2020-04-18 20:27:32 -0500

    [ADD] se anade script explicativo de como crear e invocar una funcion

---
+++ Funciones/011-crear_funciones.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Una Funcion es un conjunto de instrucciones agrupadas bajo un nombre concreto que puede reultilizarse
+invocando a la funcion tantas veces como sea necesario
+
+def nombreDeMiFuncion(parametros):
+    Conjunto de instrucciones
+
+invocacion
+
+nombreDeMiFuncion(mi_parametro)
+
+"""
+
+# Ejemplo
+
+print("\n") # Salto de linea
+print("*******Ejemplo*******")
+print("\n")
+
+# Se define la funcion
+
+def nuestraNombre():
+    print("David")
+    print("Angela")
+    print("Sarah")
+    print("Leonardo")
+    print("\n")
+
+# Se invoca la funcion
+nuestraNombre()
