9174920e4b9b56f83462ec355e6399a627035655
David Latorre <david@latorredev.com>
2020-05-16 21:54:30 -0500

    [ADD] anadido script explicativo de funciones predefinidas en python

--- Funciones/020-funciones_predefinidas.py
+++ Funciones/020-funciones_predefinidas.py
@@ -1,3 +1,54 @@
 #!/usr/bin/python3
 # Funciones predefinidas en python3
 
+nombre=input("Ingrese su nombre: ")
+
+print(type(nombre))
+
+
+# Detectar el tipado
+comprobar = isinstance(nombre, str)
+if comprobar:
+    print("Es un string")
+else:
+    print("No es un string")
+if not isinstance(nombre, float):
+    print ("La variable no es un decimal")
+
+# Limpiar espacios
+
+frase = "     mi contenido      "
+print(frase)
+print(frase.strip())
+
+# Eliminar variables
+
+year = 2020
+print(year)
+del year # Borra la variable
+
+# Comprobar variables vacias
+
+texto = " ff "
+
+if len(texto) <= 0:
+    print("La variable esta vacia")
+else:
+    print(f"la variables contiene {len(texto)} caracteres")
+
+# Encontrar caracteres
+
+frase = "La vida es una caca"
+
+print(frase.find("caca"))
+
+# Reemplazar palabras en un string
+
+nueva_frase = frase.replace("vida", "moto")
+print(nueva_frase)
+
+# Mayusculas a minusculas
+
+print(nombre)
+print(nombre.lower())
+print(nombre.upper())
