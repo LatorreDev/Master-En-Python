04a5d9fab32b91e46d17f2df2184d3c14cedcbe9
David Latorre <david@latorredev.com>
2020-05-17 18:46:31 -0500

    [ADD] anadido script explicativo de anadir un elemento a una lista en python

---
+++ Listas/03-anadir_elementos_lista.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Anadir elementos a una lista en python
+
+# Definimos la lista
+peliculas = ["Batman", "Spiderman", "El Hobbit", "Megamente", "Jumanji"]
+
+# Anadir elementos
+
+peliculas.append("Ready Player One")
+
+# Imprimimos con el nuevo elemento anadido al final
+
+print(peliculas)
