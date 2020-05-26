f84a09ea795ea131a06be1f29887c687d2222b34
David Latorre <david@latorredev.com>
2020-05-25 22:40:34 -0500

    [ADD] anadido script explicativo de diccionarios en python3

---
+++ sets/02-diccionarios.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Diccionarios en python3
+
+"""
+Es similar a una lista, pero en vez de tener un indice numerico,
+tiene un indice alfanumerico que se llama "llave" o "clave"
+"""
+
+persona = {
+"nombre":"David",
+"apellido":"Latorre",
+"web":"latorredev.com"
+}
+
+print(persona["web"])
+print(type(persona))
