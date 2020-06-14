f6cda345933ee27b78e9b5651c4909cd3d83f47d
David Latorre <david@latorredev.com>
2020-06-14 00:15:36 -0500

    [ADD] anadido main program para explicar como crear e importar paquetes propios

---
+++ paquetes/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Paquetes en python
+
+print("Probando Paquetes: ")
+
+from mipaquete import pruebas, herramientas
+
+pruebas.probando()
+herramientas.nombreCompleto("David", "Latorre")
